8cda2bfc6839c44bb89899c0d77d1a9bae36e1cc
hirata-motoi <hirata.motoi@gmail.com>
2014-03-09 17:22:16 +0900

    Modify spec of /stamp/list.json

--- lib/Babyry/Model/Stamp.pm
+++ lib/Babyry/Model/Stamp.pm
@@ -28,10 +28,23 @@ sub get_details_by_ids {
     my @stamps = ();
     while ( my $r = $itr->next ) {
         push @stamps, {
-            id       => $r->stamp_id,
-            name     => $r->stamp_name,
-            icon_url => sprintf($icon_url_base, $r->stamp_id),
+            stamp_id    => $r->stamp_id,
+            stamp_name  => $r->stamp_name,
+            icon_url    => sprintf($icon_url_base, $r->stamp_id),
         };
+    }
+
+    return \@stamps;
+}
+
+sub get {
+    my ($self, $teng) = @_;
+
+    my @stamps = ();
+
+    my $itr = $teng->search('stamp');
+    while (my $row = $itr->next) {
+        push @stamps, $row->get_columns;
     }
 
     return \@stamps;
--- lib/Babyry/Service/Stamp.pm
+++ lib/Babyry/Service/Stamp.pm
@@ -22,18 +22,42 @@ sub attach {
 sub list {
     my ($self, $user_id) = @_;
 
-    my $teng = $self->teng('BABYRY_MAIN_R');
-    my $image_user = $self->model('image_user_map');
+    my $teng       = $self->teng('BABYRY_MAIN_R');
+    my $stamp      = $self->model('stamp');
 
+    # TODO 自分のrelativesが保持しているstampのリストも返す
+    my $stamps = $stamp->get($teng);
+
+    # 各stampが何枚の写真にattachされているかを取得
+    # 自分が閲覧可能なstampのみ対象
+    my $image_count_by_stamp = $self->_get_image_count_by_stamp($teng, $user_id);
+
+    # 各stampのデータ
+    my $stamp_detail =  $stamp->get_details_by_ids($teng, [ map { $_->{stamp_id} } @$stamps ]);
+
+    # データをmerge
+    my @ret = map {
+        +{
+            %$_,
+            attached_count => $image_count_by_stamp->{ $_->{stamp_id} } || 0
+        }
+    } @$stamp_detail;
+
+    return \@ret;
+}
+
+sub _get_image_count_by_stamp {
+    my ($self, $teng, $user_id) = @_;
+
+    my $image_user = $self->model('image_user_map');
     my $image_ids = $image_user->get_image_ids_by_user_id($teng, $user_id);
     my $image_stamp_map = Babyry::Model::ImageStampMap::get_by_image_ids($teng, $image_ids);
 
-    my @stamp_ids = ();
+    my %image_count_by_stamp = ();
     for my $image_id ( keys %{$image_stamp_map} ) {
-        push @stamp_ids, map { $_->stamp_id } @{ $image_stamp_map->{$image_id} };
+        map { $image_count_by_stamp{$_}++ } @{ $image_stamp_map->{$image_id} };
     }
-
-    return $self->model('stamp')->get_details_by_ids($teng, \@stamp_ids);
+    return \%image_count_by_stamp;
 }
 
 sub get {
