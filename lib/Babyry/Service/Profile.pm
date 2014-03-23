76b547bdfb75e21b1d70fe12e3a757f36a9812f3
babyry <babyry@example.com>
2014-03-23 17:34:10 +0900

    fix no image bug

--- lib/Babyry/Service/Profile.pm
+++ lib/Babyry/Service/Profile.pm
@@ -7,7 +7,7 @@ use parent qw/Babyry::Service::Base/;
 
 use Babyry::Model::UserChildMap;
 use Log::Minimal;
-use Data::Dump;
+use Data::Dumper;
 
 
 sub get {
@@ -24,7 +24,11 @@ sub get {
 
     # get icon image
     my $image = $self->model('Image')->get_by_image_id($teng_r, $profile->{'icon_image_id'});
-    $profile->{'icon_image_url'} = $image->url;
+    if ($image) {
+        $profile->{'icon_image_url'} = $image->url;
+    } else {
+        $profile->{'icon_image_url'} = "";
+    }
 
     # get child data
     my $child_map_rows = $self->model('UserChildMap')->get_child_by_user_id($teng_r, $params);
